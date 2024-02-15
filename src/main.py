70a9493cb524265d77ff064a571333838c544f7d
Travis Tomer <travismtomer@gmail.com>
2024-02-15 11:49:50 -0500

    init commit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import datetime
+import requests
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-def main():
+@hydra.main(config_path="./config", config_name="dev")
+def main(cfg: DictConfig):
   # Main entry point for the pipeline.
-  print("hallo")
+  print(OmegaConf.to_yaml(cfg))
+  
 
 
 if __name__ == '__main__':
