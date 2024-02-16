29ff08fe5c42238d5bb0c437fa4154806753b943
Travis Tomer <travismtomer@gmail.com>
2024-02-16 10:39:59 -0500

    Init Api call and write to staging

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import requests
 import hydra
 from omegaconf import DictConfig, OmegaConf
@@ -8,16 +9,23 @@ def main(cfg: DictConfig):
   # Main entry point for the pipeline.
   OmegaConf.to_yaml(cfg)
 
+  # TODO: Add calss for api calls and error handling
   response = requests.get(
     url=f"{cfg.api.base_url}{cfg.api.ncaa_basketball_endpoint}",
     headers=cfg.api.headers,
     verify=False,
   )
 
-  print(response.json())
+  # Use the json module to load CKAN's response into a dictionary.
+  response_dict = json.loads(response.text)
 
+  # Construct file path
+  write_path = f"{cfg.storage.staging.file_path}{cfg.storage.staging.file_name}_{str(response_dict['day']['date'])}.{cfg.storage.staging.file_type}"
+  print(f"---->>>> {write_path}") # TODO: Convert to logger
 
-  
+  # TODO: Add error handling for file write and add class for file handling
+  with open(write_path, 'w') as f: # Write file to staging
+    json.dump(response_dict, f)
 
 
 if __name__ == '__main__':
